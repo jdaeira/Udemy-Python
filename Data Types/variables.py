4719e0f20c42e2e821219ee2f06c9869e96a2152
jdaeira <jdaeira@gmail.com>
2019-04-19 17:02:49 -0700

    added some code to variables.py

--- Data Types/variables.py
+++ Data Types/variables.py
@@ -1,1 +1,25 @@
 
+
+my_dog = 2
+
+my_dog = ["Zoey", "Oliver"]
+
+print(my_dog)
+
+print(type(my_dog))
+
+hello = "hello \nworld" #Prints world on the next line
+
+hello_two = "Hello World"
+
+print(hello)
+print(len(hello))
+
+print(hello_two)
+print(hello_two[-2])   #Prints the second from last character
+print(hello_two[0:5:1]) #start:end:step
+print(hello_two[::-1])
+
+mystring = "abcdefghijk"
+
+print(mystring[3:6])
