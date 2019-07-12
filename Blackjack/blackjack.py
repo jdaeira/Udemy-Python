d15f11610edb509624f131b0b31e9d36769a0a5a
jdaeira <jdaeira@gmail.com>
2019-07-12 15:42:24 -0700

    fixed some logic of the winning Player or Dealer

--- Blackjack/blackjack.py
+++ Blackjack/blackjack.py
@@ -5,6 +5,7 @@ from dealer import Dealer
 
 # This is the move variable that will keep tracl of Stand or Hit
 move = ""
+play = True 
 
 # Function to Deal the initial cards to the Player and Dealer
 def initial_deal(player, dealer, deck):
@@ -12,6 +13,8 @@ def initial_deal(player, dealer, deck):
     dealer.hand = deck.get_cards(1, dealer.hand)
     player.hand = deck.get_cards(1, player.hand)
     dealer.hand = deck.get_cards(1, dealer.hand)
+
+
 
 # Initialize the deck and shuffle the cards
 cards = Deck()
@@ -35,15 +38,21 @@ initial_deal(player1, dealer1, cards)
 player1.print_hand()
 print("Your score is: {}\n".format(player1.get_score()))
 
+# This is temporary till I figure out how to deal with the Aces
+if player1.get_score() > 21:
+    print("Player Busted!")
+    exit()
+
 # Prints Dealers first hand with 1st card not shown
-# Checks if the Dealer and Plater PUSH(tie)
+# Checks if the Dealer and Player PUSH(tie)
+# Checks if the Player wins Blackjack
 # Then checks if the Dealers wins BlackJack
 dealer1.print_first_hand()
-if dealer1.get_score() == 21 & player1.get_score() == 21:
+if dealer1.get_score() == 21 and player1.get_score() == 21:
     print("PUSH")
-    dealer1.print_first_hand()
+    dealer1.print_hand()
 elif player1.get_score() == 21:
-    print("Player Wins\n")
+    print("Blackjack! Player Wins!\n")
 elif dealer1.get_score() == 21:
     print("The House Wins!\n")
     dealer1.print_hand()
@@ -59,22 +68,29 @@ while move != "s":
         print("Your score is: {}\n".format(player1.get_score()))
         if player1.get_score() > 21:
             print("You Busted!\n")
-            break
-                
+            print("Dealer Wins!\n")
+            exit()    
+
+# Will get a card card from the dealer till the score is 17 or more
+dealer1.print_hand()
 while dealer1.get_score() < 17:
-    dealer1.print_hand()
     dealer1.hand = cards.get_cards(1, dealer1.hand)
     dealer1.print_hand()
     print("Dealer score is: {}\n".format(dealer1.get_score()))  
 
-
+# Check to see if the Dealer busted
 if dealer1.get_score() > 21:
     print("Dealer Busted\n")
-elif dealer1.get_score() >= 17 & dealer1.get_score() == player1.get_score():
+# Checks to see if the Final result is a Tie
+elif dealer1.get_score() <= 21 and dealer1.get_score() == player1.get_score():
     print("PUSH\n")
-elif dealer1.get_score() >= 17 & dealer1.get_score() > player1.get_score():
+# Checks to see if the Dealer Wins the game
+elif dealer1.get_score() <= 21 and dealer1.get_score() > player1.get_score():
     print("Dealer Wins!")
-elif dealer1.get_score() >= 17 & dealer1.get_score() < player1.get_score():
+# Checks to see if the Player Wins the game
+elif dealer1.get_score() <= 21 and dealer1.get_score() < player1.get_score():
+    dealer1.print_hand()
+    print(dealer1.get_score())
     print("Player Wins!")
 
 #dealer1.print_hand()
