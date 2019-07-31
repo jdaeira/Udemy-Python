ec5ed8750ac9d481eb5560380d2bf90405a8b514
jdaeira <jdaeira@gmail.com>
2019-07-30 19:06:34 -0700

    continued with the warmup.py exercises in the Capstone-Project

--- Capstone-Project/warmup.py
+++ Capstone-Project/warmup.py
@@ -17,6 +17,7 @@ def fizz_buzz(end):
 fizz_buzz(100)
 
 # Reverse a String - Enter a string and the program will reverse it and print it out.
+
 def reverse_word(word):
     return word[::-1]
 
@@ -29,14 +30,81 @@ print(reverse_word(reverse))
 # to the end of the word and an ay is affixed (Ex.: "banana" would yield anana-bay). 
 # Read Wikipedia for more information on rules.
 
+def pig_latin(sentence):
+    pass
+
 
 # Count Vowels - Enter a string and the program counts the number of vowels in the text. 
 # For added complexity have it report a sum of each vowel found.
+
+letter_list = []
+
+def vowels_found(phrase):
+    count = 0
+    for letter in phrase:
+        letter_list.append(letter)
+    for letters in letter_list:
+        if letters.lower() in 'aeiou':
+            count += 1
+    print(f"You have {count} vowels in the string!")
+
+def vowels_count():
+
+    acount = 0
+    ecount = 0
+    icount = 0
+    ocount = 0
+    ucount = 0
+    for vowel in letter_list:
+        if vowel.lower() == "a":
+            acount += 1
+        elif vowel.lower() == "e":
+            ecount += 1
+        elif vowel.lower() == "i":
+            icount += 1   
+        elif vowel.lower() == "o":
+            ocount += 1
+        elif vowel.lower() == "u":
+            ucount += 1
+    # Calls the function to print how many of each vowel
+    print_count(acount, ecount, icount, ocount, ucount)
+
+
+def print_count(a, e, i, o, u):
+    if a > 0:
+        print(f"a - {a}")
+    if e > 0:
+        print(f"e - {e}")
+    if i > 0:
+        print(f"i - {i}")
+    if o > 0:
+        print(f"o - {o}")
+    if u > 0:
+        print(f"u - {u}")
+
+
+vowels_found("I am a Owl")
+vowels_count()
 
 
 # Check if Palindrome - Checks if the string entered by the user is a palindrome. 
 # That is that it reads the same forwards as backwards like “racecar”
 
+def palindrome(word):
+    if word[::-1] == word:
+        print("The word you entered is a palindrome!")
+    else:
+        print("The word you entered is not a palindrome")
+
+palindrome("racecar")
+
 
 # Count Words in a String - Counts the number of individual words in a string. 
 # For added complexity read these strings in from a text file and generate a summary.
+
+def number_words(sentence):
+    word_count = sentence.split()
+    print(f"There are {len(word_count)} words in the sentence!")
+
+number_words("This is not a test!")
+
