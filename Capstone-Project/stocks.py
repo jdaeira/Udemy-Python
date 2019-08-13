bf9af0352e21d61a7f8e4625c9496ebf5d741e3d
jdaeira <jdaeira@gmail.com>
2019-08-13 15:34:31 -0700

    practice tkinter in GUI folder

--- Capstone-Project/stocks.py
+++ Capstone-Project/stocks.py
@@ -1,8 +1,17 @@
 
 from yahoo_fin.stock_info import *
 
+symbols = input("Enter stock symbols seperated by a space: ")
+stocks = symbols.split()
+
+for stock in stocks:
+    print(stock.upper())
+    price = float(get_live_price(stock.upper()))
+    print(round(price, 2))
+
 apple = get_live_price('AAPL')
 print(apple)
+print(get_quote_table("AAPL"))
 print(get_quote_table('AAPL')['Open'])
 
 print(get_day_gainers())
---
+++ GUI/place.py
@@ -0,0 +1,23 @@
+
+import tkinter
+from tkinter import *
+
+top = Tk()
+L1 = Label(top, text = "Physics")
+L1.place(x = 10,y = 20)
+E1 = Entry(top, bd = 5)
+E1.place(x = 60,y = 10, height = 50, width = 150 )
+L2 = Label(top,text = "Maths")
+L2.place(x = 10,y = 50)
+E2 = Entry(top,bd = 5)
+E2.place(x = 60,y = 50)
+
+L3 = Label(top,text = "Total")
+L3.place(x = 10,y = 150)
+E3 = Entry(top,bd = 5)
+E3.place(x = 60,y = 150)
+
+B = Button(top, text = "Add")
+B.place(x = 100, y = 100)
+top.geometry("350x350+10+10")
+top.mainloop()
